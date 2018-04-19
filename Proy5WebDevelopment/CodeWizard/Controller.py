345fb09e5fd05b818d942607b4cdcac2d25b9547
AlbertoTP <alberto.tapia.p@gmail.com>
2018-04-19 03:48:38 -0500

    Add views of proy5

--- Proy5WebDevelopment/CodeWizard/Controller.py
+++ Proy5WebDevelopment/CodeWizard/Controller.py
@@ -2,6 +2,10 @@
 import web
 
 urls = ('/','home',
+        '/discover','discover',
+        '/profile','profile',
+        '/settings','settings',
+        '/register','register',
         '/hello/', 'hello')
 
 app = web.application(urls, globals(), autoreload=True)
@@ -18,6 +22,22 @@ class home:
         #print (render)
         return render.MainLayout()
         #return "<h1> Hello CodeWizards</h1>"
+        
+class discover:
+    def GET(self):
+        return render.Discover()
+        
+class profile:
+    def GET(self):
+        return render.Profile()
+        
+class settings:
+    def GET(self):
+        return render.Settings()
+        
+class register:
+    def GET(self):
+        return render.Register()
         
 if __name__ == "__main__":
     app.run()
---
+++ Proy5WebDevelopment/CodeWizard/static/js/scipty.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  console.log("Loaded");
+  $.material.init();
+
+  $(document).on("submit","#register-form", function(e){
+    e.preventDefault();
+    console.log("Form Submited");
+  });
+});
