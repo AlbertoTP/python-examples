77d158be54cacd48b19979db0eacfc1934d0ebf4
AlbertoTP <alberto.tapia.p@gmail.com>
2018-04-25 00:01:51 -0500

    Add missing js files

--- Proy5WebDevelopment/CodeWizard/static/js/scipty.js
+++ Proy5WebDevelopment/CodeWizard/static/js/scipty.js
@@ -2,8 +2,17 @@ $(document).ready(function(){
   console.log("Loaded");
   $.material.init();
 
-  $(document).on("submit","#register-form", function(e){
+  $(document).on("submit", "#register-form", function(e){
     e.preventDefault();
-    console.log("Form Submited");
+    console.log("Form submitted");
+    var form = $("#register-form" ).serialize();
+    $.ajax({
+      url: '/postregistration',
+      type: 'POST',
+      data: form,
+      success: function(response){
+        console.log(response);
+      }
+    });
   });
 });
