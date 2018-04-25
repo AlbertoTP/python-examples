59e1327303ca9ca2a16c7326ecda0143aeeaa588
AlbertoTP <alberto.tapia.p@gmail.com>
2018-04-25 02:53:34 -0500

    Add login and sessions

--- Proy5WebDevelopment/CodeWizard/Controller.py
+++ Proy5WebDevelopment/CodeWizard/Controller.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 import web
-from Models import RegisterModel
-#import RegisterModel
+from Models import RegisterModel,LoginModel
+
+web.config.debug = False
 
 urls = ('/','home',
         '/discover','discover',
         '/profile','profile',
         '/settings','settings',
+        '/login','login',
+        '/check-login', 'checkLogin',
+        '/logout','logout',
         '/register','register',
         '/postregistration','postregistration',
         '/hello/', 'hello')
 
 app = web.application(urls, globals(), autoreload=True)
-render = web.template.render('Views/Templates/', base='Home')
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={'user': 'none'})
+session_data = session._initializer
+
+render = web.template.render('Views/Templates/', base='Home', globals={'session': session_data, 'current_user': session_data["user"]})
 
 #Classes/Routes
 class hello:
@@ -38,6 +45,22 @@ class settings:
     def GET(self):
         return render.Settings()
         
+class login:
+    def GET(self):
+        return render.Login()
+        
+class checkLogin:
+    def POST(self):
+        data = web.input()
+        print ("data is (post) ",data)
+        log=LoginModel.LoginModel()
+        isCorrect=log.check_user(data)
+        if isCorrect:
+            session_data['user'] = isCorrect
+            return isCorrect
+        return "error"
+        
+        
 class register:
     def GET(self):
         return render.Register()
@@ -49,6 +72,11 @@ class postregistration:
         reg_model=RegisterModel.RegisterModel()
         reg_model.insert_user(data)
         return data.username
+        
+class logout:
+    def GET(self):
+        session.kill()
+        return "success"
         
 if __name__ == "__main__":
     app.run()
